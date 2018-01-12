e0dc3ed04620eb06eea33057f702147deed9a16a
Shubham Maurya <code.shaurya@gmail.com>
2018-01-12 15:02:21 +0530

    redux dev tool extension is working

--- src/store.js
+++ src/store.js
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducer/index';
 
-const store = createStore(rootReducer);
+const middleware = [thunk];
+
+const composeEnhancers = composeWithDevTools({
+});
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+);
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
