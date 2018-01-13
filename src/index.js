524074594a8a925e75284a4e192a130c3b01053e
Shubham Maurya <code.shaurya@gmail.com>
2018-01-14 02:14:58 +0530

    Tictac is hosted by firebase :smiley:

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import * as firebase from "firebase";
+
 import { BrowserRouter } from "react-router-dom";
 import { AppContainer } from "react-hot-loader";
 import { Provider } from "react-redux";
+
 // comment
 import App from "./App";
 import store from "./store";
+
+const config = {
+  apiKey: "AIzaSyAppTWg88GC6Zea0mZzwLBVtephnO29S6Y",
+  authDomain: "tictac-shaurya.firebaseapp.com",
+  databaseURL: "https://tictac-shaurya.firebaseio.com",
+  projectId: "tictac-shaurya",
+  storageBucket: "tictac-shaurya.appspot.com",
+  messagingSenderId: "902131445856"
+};
+firebase.initializeApp(config);
 
 const render = Component => {
   ReactDOM.render(
