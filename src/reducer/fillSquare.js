5a1978fa09132f62ca19f66946d77c9a544f1e24
Shubham Maurya <code.shaurya@gmail.com>
2018-01-26 10:49:14 +0530

    Player reducers is deleted . Fill square added

--- src/reducer/fillSquare.js
+++ src/reducer/fillSquare.js
@@ -1,9 +1,11 @@
-import { board } from '../data/board';
+import { board ,winner,xNext} from '../data/board';
 import { player1 } from '../data/player';
 import { player2 } from '../data/player';
 
 const initialState = {
   board,
+  xNext,
+  winner,
   player1,
   player2,
 }
@@ -12,10 +14,35 @@ function fillSquare(state = initialState, action) {
   switch (action.type) {
     case 'FILL_SQUARE':
       {
-        const { index } = action;
+        const { index,xNext } = action.payload;
+        let newState={...state};
 
-        return state;
+        let arr= state.board;
+
+        arr[index]=xNext ?state.player1.code : state.player2.code;
+          newState={
+            ...newState,
+            xNext:!state.xNext,
+            board:arr,
+          }
+
+        return newState;
       }
+
+
+       case 'PLAYER_INFO':
+      {
+        const { player1,player2} = action.payload;
+        let newState={...state};
+        newState ={
+          ...newState,
+          player1,
+          player2,              
+        }
+
+        return newState;
+      }
+
     default:
       return state
   }
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
 
 import fillSquare from './fillSquare';
-import player from './player';
-
 
 const rootReducer = combineReducers({
   fillSquare,
-  player,
 });
 
 export default rootReducer;
